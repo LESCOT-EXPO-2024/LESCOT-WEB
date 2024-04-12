626961ede4b4ac6464413873db0a62074da7fcad
STEVEN ALEXANDER ALPIZAR GAMBOA <s.alpizar207@gmail.com>
2024-04-12 14:36:39 -0600

    Update icons in Astro configuration and page content

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,6 +40,7 @@ export default defineConfig({
       include: {
         tabler: ['*'],
         'flat-color-icons': [
+          'electronics',
           'template',
           'gallery',
           'approval',
