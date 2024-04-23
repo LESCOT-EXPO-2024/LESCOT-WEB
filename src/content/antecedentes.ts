cec565612078eb4b3dcaff1c3698d4d063af0616
STEVEN ALEXANDER ALPIZAR GAMBOA <s.alpizar207@gmail.com>
2024-04-22 21:09:41 -0600

    Update image links in antecedentes.ts and index.astro

--- src/content/antecedentes.ts
+++ src/content/antecedentes.ts
@@ -21,7 +21,7 @@ const antecedentes: antecedente[] = [
         subtitulo: 'IncluTec',
         link: 'https://www.tec.ac.cr/traductor-lesco',
         descripcion: 'PIELS buscaba realizar un traductor de texto a LESCO, pero al realizar el avatar en la plataforma Unity notaron que la programación era elaborada, así que, decidieron simplificar la acción de programar cada seña que hiciera el avatar. Inclu- TEC, grupo de investigación de interés de la escuela de ingeniería en computación, se ha dedicado a investigar y aplicar tecnologías que ofrezcan facilidades para las personas con discapacidad.',
-        imageSrc: 'https://www.tec.ac.cr/hoyeneltec/sites/default/files/media/img/main/avatar._lesco_.png',
+        imageSrc: 'https://i.ytimg.com/vi/tvRJh1PdbWU/maxresdefault.jpg',
         imageAlt: 'PIELS INCLUTEC'
     },
     {
@@ -29,7 +29,7 @@ const antecedentes: antecedente[] = [
         subtitulo: 'SignAll Technologies Inc',
         link: 'https://ace-asl-learn-fingerspelling.softonic.com/android',
         descripcion: 'Es una aplicación gratuita desarrollada por SignAl Technologies Inc., tiene el fin de enseñar la Lengua de Señas Americana (ASL). Esta consta con una parte interactiva en ella se puede practicar las la lengua con una persona sorda y una persona oyente, a través del uso de la cámara del teléfono.',
-        imageSrc: 'https://images.sftcdn.net/images/t_app-icon-m/p/e2eb5825-1ae7-4bf3-a1e7-3aded6f3d895/2983533438/ace-asl-learn-fingerspelling-logo',
+        imageSrc: 'https://ph-files.imgix.net/0076d9d4-b7af-4e1c-9a7c-80f49392d0c3.jpeg?auto=format&fit=crop',
         imageAlt: 'ACE ASL'
     },
     {
@@ -45,7 +45,7 @@ const antecedentes: antecedente[] = [
         subtitulo: '@Sign_Speak_',
         link: 'https://www.sign-speak.com/',
         descripcion: 'Sign Speak es una aplicación móvil que utiliza inteligencia artificial para traducir el lenguaje de señas al lenguaje hablado. La aplicación fue creada por Yamilet Payano, una emprendedora sorda, con el objetivo de hacer la comunicación más accesible para las personas sordas o con problemas de audición. La aplicación utiliza una combinación de visión artificial y aprendizaje automático para identificar y traducir gestos en lenguaje de señas. El usuario simplemente utiliza la cámara del dispositivo y la aplicación mostrará el texto correspondiente en la pantalla, de igual manera se encuentra en desarrollo.',
-        imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEpwvjuCiCE54hT38vXKZy3pGFD6gbNbG4W9nHmiKT_Q&s',
+        imageSrc: 'https://elcomercio.pe/resizer/nqgouXT6X5s0E6FLjl3JmQly7MQ=/620x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/G36MF5GLVJGKXAQYYBKLRDMFQM.jpg',
         imageAlt: 'SIGN SPEAK'
     },
 ];
