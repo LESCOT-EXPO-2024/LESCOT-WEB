c4941f3be04c25d3d3b4b06c862b64588d40fa87
STEVEN ALEXANDER ALPIZAR GAMBOA <s.alpizar207@gmail.com>
2024-09-08 18:20:14 -0600

    chore: Update font sizes, file paths, and font family for CustomStyles component

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routes: {
+        en:{
+            abecedario: "alphabet",
+            contacto: "contact",
+            index: "home",
+            equipo: "team",
+            lenguasdeseña: "signlanguage",
+        }
+    }
+  };
